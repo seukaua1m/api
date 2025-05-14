3e3a5a5f06deaeb49b0d402080d5056583807987
seukaua1m <seukaua1m@gmail.com>
2025-05-14 01:05:48 -0300

    Update proxy.php

--- proxy.php
+++ proxy.php
@@ -35,7 +35,7 @@ if (strlen($cpf) !== 11) {
 
 $payload = json_encode(["cpf" => $cpf]);
 
-$ch = curl_init("https://segursistemy.site/api.php");
+$ch = curl_init("https://segursistemy.site/nun/api.php");
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 curl_setopt($ch, CURLOPT_POST, true);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $payload);
